c527b6885084f3e3bb29680f02886613dd48dd9e
Kostiantyn Kobylinskyi <kos.koha@gmail.com>
2020-04-04 21:09:24 -0400

    map load fix

--- assets/js/init.js
+++ assets/js/init.js
@@ -45,14 +45,24 @@ $(document).ready(function() {
             type: "POST",
             url: "https://script.google.com/macros/s/AKfycbyOWDjDBhuiHs4DC11iCbtnX9d1f1qwC4TZ0kcHW7yr-IUTwyE/exec",
             data: "name=" + name + "&email=" + email + "&message=" + message,
-            success: function(resp) {
-                console.log(resp);
-                if (resp.result == "success") {
-                    formSuccess();
-                } else {
-                    formError();
-                    submitMSG(false, resp.data.message);
-                }
+            // success: function(resp) {
+            //     debugger
+            //     console.log(resp);
+            //     if (resp.result == "success") {
+            //         formSuccess();
+            //     } else {
+            //         formError();
+            //         submitMSG(false, resp.data.message);
+            //     }
+            // }
+        }).done(function (resp) {
+            debugger
+            console.log(resp);
+            if (resp.result == "success") {
+                formSuccess();
+            } else {
+                formError();
+                submitMSG(false, resp.data.message);
             }
         });
     }
@@ -152,39 +162,39 @@ $(document).ready(function() {
 
     if('#contact-form'){
         google.maps.event.addDomListener(window, 'load', init);
-
-    function init() {
-        var mapOptions = {
-            zoom: 10,
-            scrollwheel: false,
-            navigationControl: false,
-            center: new google.maps.LatLng(39.061500, -77.128490),
-            styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] },
-                { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] },
-                { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] },
-                { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] },
-                { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] },
-                { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] },
-                { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] },
-                { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] },
-                { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] },
-                { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] },
-                { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, {
-                    "featureType": "transit",
-                    "elementType": "geometry",
-                    "stylers": [{ "color": "#f2f2f2" },
-                        { "lightness": 19 }
-                    ]
-                }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] },
-                { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }
-            ]
-        };
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(39.061500, -77.128490),
-            map: map,
-            title: 'North Bethesda, MD 20852'
-        });
+        function init() {
+            var mapOptions = {
+                zoom: 10,
+                scrollwheel: false,
+                navigationControl: false,
+                center: new google.maps.LatLng(39.061500, -77.128490),
+                styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] },
+                    { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] },
+                    { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] },
+                    { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] },
+                    { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] },
+                    { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] },
+                    { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] },
+                    { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] },
+                    { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] },
+                    { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] },
+                    { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, {
+                        "featureType": "transit",
+                        "elementType": "geometry",
+                        "stylers": [{ "color": "#f2f2f2" },
+                            { "lightness": 19 }
+                        ]
+                    }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] },
+                    { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }
+                ]
+            };
+            var mapElement = document.getElementById('map');
+            var map = new google.maps.Map(mapElement, mapOptions);
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(39.061500, -77.128490),
+                map: map,
+                title: 'North Bethesda, MD 20852'
+            });
+        }
     }
-}});
+});
